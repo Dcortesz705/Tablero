936dec9a95555ecac7d550414ec00e6b0fb21bb5
Daniela <dcortesz705@gmail.com>
2024-10-03 10:33:01 -0500

    Update App.py

--- App.py
+++ App.py
@@ -1,16 +1,18 @@
-import streamlit as st 
-st.title('dibujeeee')
+import streamlit as st
+from streamlit_drawable_canvas import st_canvas
 
-
-# Add canvas component
-# Specify canvas parameters in application
-drawing_mode = "freedraw"
-stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
-stroke_color = '#FFFFFF' # Set background color to white
+st.title("¡Deja volar tu imaginación y dibuja!")
 bg_color = '#000000'
 
-# Create a canvas component
-st_canvas(
+with st.sidebar:
+    stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
+    drawing_mode = st.selectbox(
+    "How would you like to be contacted?",
+    ("freedraw", "line", "rect"),
+    )
+    stroke_color = st.color_picker("Pick A Color", "#00f900")
+    
+canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
     stroke_width=stroke_width,
     stroke_color=stroke_color,
